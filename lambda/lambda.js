61a34c4383aa523c7fcfb56147d7181a123d81dc
Franco Torriani <ftorriani@facilitylive.com>
2017-06-02 22:41:12 +0200

    Lambda function

---
+++ lambda/lambda.js
@@ -0,0 +1,23 @@
+var AWS = require('aws-sdk');
+var iotdata = new AWS.IotData({endpoint: 'a23om0asc4d73b.iot.eu-west-1.amazonaws.com'});
+exports.handler = function(event, context) {
+ 
+    var params = {
+        topic: 'aws-in-topic',
+        payload: '{"action" : "TOGGLE_LIGHT"}',
+        qos: 0
+        };
+
+
+    iotdata.publish(params, function(err, data){
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log("success?");
+            //context.succeed(event);
+        }
+    });
+
+    return 'OK'
+};
